95de1827a4af12c1627c5a5dd826eaa444058333
Vivek Sharma <viveks@Viveks-MacBook-Pro.local>
2021-05-25 00:13:42 +0530

    Backend working, need to make view work

--- src/app/view-form/view-form.component.ts
+++ src/app/view-form/view-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {UserServiceService} from '../service/user-service.service';
+import { environment } from '../../environments/environment';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import {AngularFireAuth} from '@angular/fire/auth';
+
 
 @Component({
   selector: 'app-view-form',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserServiceService,private http: HttpClient,public auth: AngularFireAuth,) { 
+    this.getFormData();
+  }
 
   ngOnInit(): void {
   }
 
-}
+
+  getFormData(){
+    const APIendpoint = environment.APIEndpoint;
+    const posturl=APIendpoint+'/api/createsignpdf/getdocstatus';
+    let usridtoken:any;
+
+    this.auth.idToken.subscribe((res)=>{
+      usridtoken=res;
+
+      let httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+          'Access-Control-Allow-Origin':'*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT',
+          'authorization': usridtoken
+        })
+      };//end of httpoptions
+
+      this.http.get<any>(posturl, httpOptions).subscribe(
+        (res)=> {
+        
+          console.log("res is "+JSON.stringify(res));
+        } ,
+        (err)=> {
+          
+          console.log(err.error.message);
+        }
+      );
+
+    });
+
+  }//end of getFormData
+}//end of class
